d6d1a2ecbe5f10559b705458245a1926777bdd7b
saikeerthi8586 <rompallysaikeerthi@gmail.com>
2025-06-28 09:22:00 +0000

    day18

---
+++ day18/functions.js
@@ -0,0 +1,16 @@
+function myfun(){
+    console.log("Welcome");
+}
+myfun();
+
+function add(){
+    var a=10;
+    var b=23;
+    console.log(a+b);
+}
+add();
+
+function addTwo(num1,num2){
+    console.log(num1+num2);
+}
+addTwo(12,34);
