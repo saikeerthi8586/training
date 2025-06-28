5581e9ee1f742f28b4d6417d49bf919fe9941b49
saikeerthi8586 <rompallysaikeerthi@gmail.com>
2025-06-28 09:21:02 +0000

    day17

---
+++ day17/script.js
@@ -0,0 +1,67 @@
+// //  document.write("Welcome");
+// //         /*window.alert("Welcome");*/
+// //         console.log("Welcome");
+// //         document.getElementById("h2").innerHTML="welcome to the page";
+
+// // var a = 2;
+// // var b = 4;
+// // var c = '6';
+// // console.log(a + b + c); 
+// // console.log(a + c + b); 
+// // document.write(a+b+c);
+// // document.writeln(a+c+b);
+// var num=10;
+// if(num%2==0){
+//     document.write("Even");
+// }
+// else{
+//     document.write("Odd");
+// }
+
+// var a = "w";
+
+// if (a == "a" || a == "e" || a == "i" || a == "o" || a == "u") {
+//     document.write("Vowel");
+// } else {
+//     document.write("Consonant");
+// }
+
+
+
+
+// given day is weekday or weekend:
+// var day="monday";
+// if(day=="monday"||day=="tuesday"||day=="wednesday"||day=="thursday"||day=="friday"){
+//     document.write("Week day");
+// }
+// else{
+//     document.write("Week end");
+// }
+
+
+
+// given number  is below 50 or 50 or above 50:
+// var num=60;
+// if(num<50){
+//     document.write("Im less than 50");
+// }
+// else if(num>50){
+//     document.write("im between 50 and 100");
+// }
+// else{
+//     document.write("Im above 100");
+// }
+
+
+// greatest of three numbers:
+// var num1 = 12;
+// var num2 = 34;
+// var num3 = 1;
+
+// if (num1 > num2 && num1 > num3) {
+//     document.write("Num 1 is greatest");
+// } else if (num2 > num1 && num2 > num3) {
+//     document.write("Num 2 is greatest");
+// } else {
+//     document.write("Num 3 is greatest");
+// }
