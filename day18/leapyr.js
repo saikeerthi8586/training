b3525dbe6ab2c47d91cf8f90bf4794adff46a14d
saikeerthi8586 <rompallysaikeerthi@gmail.com>
2025-06-28 09:24:29 +0000

    day18

---
+++ day18/leapyr.js
@@ -0,0 +1,7 @@
+//  printing that the year is leap year or not:
+var year = 2024;
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+  console.log(year + " is a Leap Year");
+} else {
+  console.log(year + " is NOT a Leap Year");
+}
